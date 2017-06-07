b096cd2ba157204a834d0b19a1a1b0170d588f8a
Mainul Hossain <mainulhossain@gmail.com>
2017-06-07 12:07:36 +0600

    register image

--- func_resolver.py
+++ func_resolver.py
@@ -54,7 +54,7 @@ def call_func(context, module_name, func_name, arguments):
         elif func_name.lower() == "convert_color":
             return ImageProcessor.convert_color(path.join(localdir, arguments[0]), path.join(localdir, arguments[1]), arguments[2])
         elif func_name.lower() == "register_image":
-            return ImageProcessor.register_image(path.join(localdir, arguments[0]), path.join(localdir, arguments[1]))
+            return ImageProcessor.register_image(path.join(localdir, arguments[0]), path.join(localdir, arguments[1]), path.join(localdir, arguments[2]))
         else:
             raise "{0} function not implemented".format(func_name)
 #             possibles = globals().copy()
--- imgproc/imagefuncs.py
+++ imgproc/imagefuncs.py
@@ -26,52 +26,45 @@ class ImageProcessor():
         return grad
     
     @staticmethod
-    def register_image(srcfile, destfile):
-        # Read 8-bit color image.
-        # This is an image in which the three channels are
-        # concatenated vertically.
-     
-        im =  cv2.imread(srcfile, cv2.IMREAD_GRAYSCALE);
-     
-        # Find the width and height of the color image
-        sz = im.shape
-        height = int(sz[0] / 3);
-        width = sz[1]
-     
-        # Extract the three channels from the gray scale image
-        # and merge the three channels into one color image
-        im_color = np.zeros((height,width,3), dtype=np.uint8 )
-        for i in xrange(0,3) :
-            im_color[:,:,i] = im[ i * height:(i+1) * height,:]
-     
-        # Allocate space for aligned image
-        im_aligned = np.zeros((height,width,3), dtype=np.uint8 )
-     
-        # The blue and green channels will be aligned to the red channel.
-        # So copy the red channel
-        im_aligned[:,:,2] = im_color[:,:,2]
-     
-        # Define motion model
-        warp_mode = cv2.MOTION_HOMOGRAPHY
-     
-        # Set the warp matrix to identity.
-        if warp_mode == cv2.MOTION_HOMOGRAPHY:
+    def register_image(srcfile1, srcfile2, destfile):
+        # Read the images to be aligned
+        im1 =  cv2.imread(srcfile1);
+        im2 =  cv2.imread(srcfile1);
+         
+        # Convert images to grayscale
+        im1_gray = cv2.cvtColor(im1,cv2.COLOR_BGR2GRAY)
+        im2_gray = cv2.cvtColor(im2,cv2.COLOR_BGR2GRAY)
+         
+        # Find size of image1
+        sz = im1.shape
+         
+        # Define the motion model
+        warp_mode = cv2.MOTION_TRANSLATION
+         
+        # Define 2x3 or 3x3 matrices and initialize the matrix to identity
+        if warp_mode == cv2.MOTION_HOMOGRAPHY :
             warp_matrix = np.eye(3, 3, dtype=np.float32)
-        else:
+        else :
             warp_matrix = np.eye(2, 3, dtype=np.float32)
-     
-        # Set the stopping criteria for the algorithm.
-        criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 5000,  1e-10)
-     
-        # Warp the blue and green channels to the red channel
-        for i in xrange(0,2) :
-            (cc, warp_matrix) = cv2.findTransformECC (ImageProcessor.get_gradient(im_color[:,:,2]), ImageProcessor.get_gradient(im_color[:,:,i]), warp_matrix, warp_mode, criteria)
-     
-            if warp_mode == cv2.MOTION_HOMOGRAPHY :
-                # Use Perspective warp when the transformation is a Homography
-                im_aligned[:,:,i] = cv2.warpPerspective (im_color[:,:,i], warp_matrix, (width,height), flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
-            else :
-                # Use Affine warp when the transformation is not a Homography
-                im_aligned[:,:,i] = cv2.warpAffine(im_color[:,:,i], warp_matrix, (width, height), flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP);
+         
+        # Specify the number of iterations.
+        number_of_iterations = 5000;
+         
+        # Specify the threshold of the increment
+        # in the correlation coefficient between two iterations
+        termination_eps = 1e-10;
+         
+        # Define termination criteria
+        criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, number_of_iterations,  termination_eps)
+         
+        # Run the ECC algorithm. The results are stored in warp_matrix.
+        (cc, warp_matrix) = cv2.findTransformECC (im1_gray,im2_gray,warp_matrix, warp_mode, criteria)
+         
+        if warp_mode == cv2.MOTION_HOMOGRAPHY :
+            # Use warpPerspective for Homography 
+            im2_aligned = cv2.warpPerspective (im2, warp_matrix, (sz[1],sz[0]), flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP)
+        else :
+            # Use warpAffine for Translation, Euclidean and Affine
+            im2_aligned = cv2.warpAffine(im2, warp_matrix, (sz[1],sz[0]), flags=cv2.INTER_LINEAR + cv2.WARP_INVERSE_MAP);
         
-        cv2.imwrite(dstfile, im_aligned)
+        cv2.imwrite(dstfile, im2_aligned)
