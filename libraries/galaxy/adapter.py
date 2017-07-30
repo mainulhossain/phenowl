6748ae71560afa11c10407d4769b17fa80296cd1
mainulhossain <mainulhossain@gmail.com>
2017-07-30 00:23:24 -0600

    bioblend adjustment

--- libraries/galaxy/adapter.py
+++ libraries/galaxy/adapter.py
@@ -1,19 +1,20 @@
 from bioblend.galaxy import GalaxyInstance
 import json
 
-def get_workflows(*args):
+def get_workflows_json(*args):
     gi = GalaxyInstance(args[0], args[1])
-    wf = gi.workflows.get_workflows()
-    js = json.load(wf)
+    return gi.workflows.get_workflows()
+    
+def get_workflows(*args):
+    wf = get_workflows_json(*args)
     wf_names = []
-    for j in js:
-        wf_names.append(js.name)
+    for j in wf:
+        #yield j.name
+        wf_names.append(j['name'])
     return wf_names
 
 def get_workflow(*args):
-    gi = GalaxyInstance(args[0], args[1])
-    wf = gi.workflows.get_workflows()
-    js = json.load(wf)
-    for j in js:
-        if js.name is args[2]:
-            return js
+    wf = get_workflows_json(args)
+    for j in wf:
+        if j['name'] is args[2]:
+            return j
