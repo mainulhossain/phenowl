cc10b9b72ec54d4409a6f1de6187e469285f60fa
mainulhossain <mainulhossain@gmail.com>
2017-08-01 08:25:35 -0600

    galaxy

--- libraries/galaxy/adapter.py
+++ libraries/galaxy/adapter.py
@@ -5,16 +5,67 @@ def get_workflows_json(*args):
     gi = GalaxyInstance(args[0], args[1])
     return gi.workflows.get_workflows()
     
-def get_workflows(*args):
+def get_workflow_ids(*args):
     wf = get_workflows_json(*args)
-    wf_names = []
+    wf_ids = []
     for j in wf:
         #yield j.name
-        wf_names.append(j['name'])
-    return wf_names
+        wf_ids.append(j['id'])
+    return wf_ids
 
 def get_workflow(*args):
-    wf = get_workflows_json(args)
+    wf = get_workflows_json(*args)
     for j in wf:
-        if j['name'] is args[2]:
+        if j['id'] == args[3]:
             return j
+        
+def get_libraries_json(*args):
+    gi = GalaxyInstance(args[0], args[1])
+    return gi.libraries.get_libraries()
+    
+def get_library_ids(*args):
+    wf = get_libraries_json(*args)
+    wf_ids = []
+    for j in wf:
+        #yield j.name
+        wf_ids.append(j['id'])
+    return wf_ids
+
+def get_library(*args):
+    wf = get_libraries_json(*args)
+    for j in wf:
+        if j['id'] == args[3]:
+            return j
+        
+def get_histories_json(*args):
+    gi = GalaxyInstance(args[0], args[1])
+    return gi.histories.get_histories()
+    
+def get_history_ids(*args):
+    wf = get_histories_json(*args)
+    wf_ids = []
+    for j in wf:
+        #yield j.name
+        wf_ids.append(j['id'])
+    return wf_ids
+
+def get_history(*args):
+    wf = get_histories_json(*args)
+    for j in wf:
+        if j['id'] == args[3]:
+            return j
+                
+def upload(*args):
+    gi = GalaxyInstance(args[0], args[1])
+    library = get_library(*args)
+    if library is not None:
+        return gi.libraries.upload_file_from_local_path(library['id'], args[4])
+    else:
+        r = gi.tools.upload_file(args[4], args[3])
+    
+def run_workflow(*args):
+    gi = GalaxyInstance(args[0], args[1])
+    workflow_id = args[3]
+    datamap = dict()
+    datamap['252'] = { 'src':'hda', 'id':str(args[4]) }
+    return gi.workflows.run_workflow(args[3], datamap, history_name='New Workflow Execution History')
