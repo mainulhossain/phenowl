bb1350ccda2dec48f7c1f1ab159b29820ca10b1e
Mainul Hossain <mainulhossain@gmail.com>
2017-09-03 22:56:01 +0600

    vsearch

--- libraries/vsearch/adapter.py
+++ libraries/vsearch/adapter.py
@@ -3,7 +3,7 @@ from os import path
 from exechelper import func_exec_run
 
 localdir = path.join(path.dirname(path.dirname(path.dirname(__file__))), 'storage')
-usearch = path.join(path.abspath(path.dirname(__file__)), path.join('bin', 'usearch10.0.240_i86linux32'))
+vsearch = 'vsearch' # vsearch must be in the path
 
 def run_usearch(*args):
     cmdargs = ["-" + args[0]]
@@ -19,7 +19,7 @@ def run_usearch(*args):
     for arg in args[4:]:
         cmdargs.append(arg)
     
-    func_exec_run(usearch, *cmdargs)
+    func_exec_run(vsearch, *cmdargs)
     
     return output_file
     
